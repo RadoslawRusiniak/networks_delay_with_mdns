a9c58292b6cfbff8264c2a247478416634e77c7d
Radosław Rusiniak <radoslaw.rusiniak@gmail.com>
2015-08-12 03:15:52 +0200

    Sending ptr question via multicast.

--- mdns.c
+++ mdns.c
@@ -1,32 +1,75 @@
 
 #include "mdns.h"
 
-void send_mcast_data(evutil_socket_t sock, short ev, void *arg) {
-  fprintf(stderr, "Sending multicast data\n");
-  char buffer[BUFSIZE];
-  size_t length;
-  strncpy(buffer, "timer_cb data", BUFSIZE);
-  length = strnlen(buffer, BUFSIZE);
-  if (write(sock, buffer, length) != length) {
+void append_in_dns_name_format(unsigned char* dns, unsigned char* host)
+{
+  //dns name format: 3wwwm5imuw3edu2pl0 
+  int lock = 0 , i;
+  strcat((char*)host, ".");
+
+  for (i = 0; i < strlen((char*)host); ++i) {
+    if (host[i]=='.') {
+      *dns++ = i-lock;
+      for( ; lock<i; ++lock) {
+        *dns++ = host[lock];
+      }
+      ++lock;
+    }
+  }
+  *dns++='\0';
+}
+
+void prepare_dns_question_header(struct DNS_HEADER *dns) {
+  memset(dns, 0, sizeof(struct DNS_HEADER));
+  dns->q_count = htons(1);
+}
+
+void send_mcast_data(evutil_socket_t sock, short events, void *arg) {
+  fprintf(stderr, "Sending PTR question via multicast\n");
+  char buf[BUFSIZE];
+  
+  struct DNS_HEADER *dns = (struct DNS_HEADER *)&buf;
+  prepare_dns_question_header(dns);
+  
+  unsigned char * qname = (unsigned char *)&buf[sizeof(struct DNS_HEADER)];
+  unsigned char service[64] = DNS_DISCOVERY_RR;
+  append_in_dns_name_format(qname, service);
+  
+  struct QUESTION *qinfo = (struct QUESTION *)
+      &buf[sizeof(struct DNS_HEADER) + (strlen((const char*)qname) + 1)];
+  qinfo->qtype = htons(T_PTR); //type of the query
+  qinfo->qclass = htons(1); //its internet
+  
+  size_t length = sizeof(struct DNS_HEADER) + (strlen((const char*)qname)+1) + sizeof(struct QUESTION);
+  if (write(sock, buf, length) != length) {
     syserr("write");
   }
-  fprintf(stderr, "Data sent\n");
+  fprintf(stderr, "PTR question via multicast sent\n");
 }
 
 void read_mcast_data(evutil_socket_t sock, short events, void *arg)
 {
-  char buf[1024];
-  
-//    struct evbuffer *input = bufferevent_get_input(bev);
+  char buf[BUFSIZE];
   struct sockaddr_in src_addr;
   socklen_t len;
   ssize_t result;
   result = recvfrom(sock, buf, sizeof(buf), 0, (struct sockaddr*)&src_addr, &len);
-  fprintf(stderr, "recv %zd from %s:%d\n", result, inet_ntoa(src_addr.sin_addr), ntohs(src_addr.sin_port));
+  fprintf(stderr, "\nReceived %zd bytes from %s:%d\n", 
+          result, inet_ntoa(src_addr.sin_addr), ntohs(src_addr.sin_port));
+  
   if(result < 0) {
     fatal("Receiving data from multicast.");
   } else if (result == 0) {
     fprintf(stderr, "Connection closed.\n");
     if(close(sock) == -1) syserr("Error closing socket.");
   }
+  
+  struct DNS_HEADER * dns = NULL;
+  dns = (struct DNS_HEADER *) &buf;
+
+  printf("The response contains : ");
+  printf("\n %d Questions.", ntohs(dns->q_count));
+  printf("\n %d Answers.", ntohs(dns->ans_count));
+  printf("\n %d Authoritative Servers.", ntohs(dns->auth_count));
+  printf("\n %d Additional records.\n", ntohs(dns->add_count));
 }
--- mdns.h
+++ mdns.h
@@ -19,9 +19,42 @@
 
 #include "err.h"
 
-#define BUFSIZE       1024
+#define BUFSIZE       65536
 
-void send_mcast_data(evutil_socket_t sock, short ev, void *arg);
+#define T_A 1
+#define T_PTR 12
+
+#define DNS_DISCOVERY_RR   "_services._dns-sd._udp.local"
+
+//DNS header structure
+struct DNS_HEADER
+{
+  unsigned short id; // identification number
+ 
+  unsigned char qr :1; // query/response flag
+  unsigned char opcode :4; // purpose of message
+  unsigned char aa :1; // authoritive answer
+  unsigned char tc :1; // truncated message
+  unsigned char rd :1; // recursion desired
+  unsigned char ra :1; // recursion available
+
+  unsigned char z :3; // reserved for future purposes
+  unsigned char rcode :4; // response code
+ 
+  unsigned short q_count; // number of question entries
+  unsigned short ans_count; // number of answer entries
+  unsigned short auth_count; // number of authority entries
+  unsigned short add_count; // number of resource entries
+};
+
+//Constant sized fields of query structure
+struct QUESTION
+{
+  unsigned short qtype;
+  unsigned short qclass;
+};
+
+void send_mcast_data(evutil_socket_t sock, short events, void *arg);
 
 void read_mcast_data(evutil_socket_t sock, short events, void *arg);
 
