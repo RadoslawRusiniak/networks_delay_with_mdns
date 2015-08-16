be0b1d5a4da934a97b1ec16150e0016830a9138b
Radosław Rusiniak <radoslaw.rusiniak@gmail.com>
2015-08-16 17:29:56 +0200

    Refactoring.

--- mdns.c
+++ mdns.c
@@ -1,9 +1,5 @@
 #include "mdns.h"
 
-void append_in_dns_name_format(char * dns, char * host);
-
-char * read_name_from_packet(char * read_pointer, char * buffer, int * count);
-
 void send_PTR_query(evutil_socket_t sock, short events, void *arg) {
   fprintf(stderr, "Sending PTR question via multicast.\n");
   char buf[BUF_SIZE];
@@ -21,7 +17,7 @@ void send_PTR_query(evutil_socket_t sock, short events, void *arg) {
   qinfo->qtype = htons(T_PTR); //type of the query
   qinfo->qclass = htons(1); //its internet
 
-  size_t length = sizeof(struct DNS_HEADER) + (sizeof(MDNS_SERVICE) + 1) + sizeof(struct QUESTION);
+  size_t length = sizeof (struct DNS_HEADER) + (sizeof (MDNS_SERVICE) + 1) + sizeof (struct QUESTION);
   if (write(sock, buf, length) != length) {
     syserr("write");
   }
@@ -31,38 +27,125 @@ void send_PTR_query(evutil_socket_t sock, short events, void *arg) {
 void send_PTR_answer(evutil_socket_t sock, short events, void *arg) {
   fprintf(stderr, "Sending PTR answer via multicast.\n");
   char buf[BUF_SIZE];
-  
+
   struct DNS_HEADER *dns = (struct DNS_HEADER *) &buf;
   memset(dns, 0, sizeof (struct DNS_HEADER));
   dns->ans_count = htons(1);
-  
+
   ssize_t frame_len = sizeof (struct DNS_HEADER);
   struct RES_RECORD *answer = (struct RES_RECORD *) &buf[frame_len];
   answer->name = &buf[frame_len];
-  
-  char hostname[256]; char * host_pointer = hostname;
+
+  char hostname[256];
+  char * host_pointer = hostname;
   get_hostname(host_pointer);
   fprintf(stderr, "\thostname: %s\n", host_pointer);
-//  char service_suf[64] = MDNS_SERVICE_SUFFIX;
+  //  char service_suf[64] = MDNS_SERVICE_SUFFIX;
   strcat(host_pointer, MDNS_SERVICE_SUFFIX);
-//  hostname[strlen(host_pointer)] = '\0';
+  //  hostname[strlen(host_pointer)] = '\0';
   fprintf(stderr, "\thostname with suffix: %s\n", host_pointer);
   append_in_dns_name_format(answer->name, host_pointer);
-  
+
   frame_len += strlen(host_pointer) + 1;
-  
+
   answer->resource = (struct R_DATA *) &buf[frame_len];
   answer->resource->rtype = htons(T_PTR);
   answer->resource->rclass = htons(1);
   answer->resource->ttl = htons(255);
   answer->resource->data_len = 0;
-  
+
   frame_len += sizeof (struct R_DATA);
-  
+
   if (write(sock, buf, frame_len) != frame_len) {
     syserr("write");
   }
   fprintf(stderr, "PTR answer via multicast sent\n");
+}
+
+void handle_PTR_question(struct event_base *base) {
+  struct event *an_event =
+          event_new(base, write_MDNS_sock, EV_WRITE, send_PTR_answer, NULL);
+  if (!an_event) syserr("Error creating event.");
+  if (event_add(an_event, NULL) == -1) syserr("Error adding an event to a base.");
+}
+
+void handle_A_question(struct QUERY * question) {
+
+}
+
+void handle_PTR_answer(struct RES_RECORD * answer) {
+
+}
+
+void handle_A_answer(struct RES_RECORD * answer) {
+
+}
+
+void handle_questions(struct DNS_HEADER * dns, char * read_pointer, char * buf, int * consumed, 
+        struct event_base *base) {
+  
+  struct QUERY questions[10];
+  int i;
+  for (i = 0; i < ntohs(dns->q_count); i++) {
+    fprintf(stderr, " Question nr %d analyzed:\n", i + 1);
+    questions[i].name = read_name_from_packet(read_pointer, consumed);
+    read_pointer += *consumed;
+    fprintf(stderr, "\tName: %s\n", questions[i].name);
+
+    questions[i].ques = (struct QUESTION *) (read_pointer);
+    if (ntohs(questions[i].ques->qtype) == T_PTR) {
+      fprintf(stderr, "\tT_PTR question.\n");
+      handle_PTR_question(base);
+    }
+    else if (ntohs(questions[i].ques->qtype) == T_A) {
+      fprintf(stderr, "\tT_A question.\n");
+      handle_A_question(&questions[i]);
+    }
+    else {
+      fprintf(stderr, "Unsupported or unknown resource type\n.\
+                        Next resource records in this frame skipped\n");
+      return;
+    }
+  }
+}
+
+void handle_answers(struct DNS_HEADER * dns, char * read_pointer, char * buf, int * consumed, 
+        struct event_base *base) {
+  
+  struct RES_RECORD answers[10];
+  int i, j;
+  for (i = 0; i < ntohs(dns->ans_count); i++) {
+    fprintf(stderr, " Answer nr %d analyzed:\n", i + 1);
+    answers[i].name = read_name_from_packet(read_pointer, consumed);
+    read_pointer += *consumed;
+    fprintf(stderr, "\tName: %s\n", answers[i].name);
+
+    answers[i].resource = (struct R_DATA *) (read_pointer);
+    read_pointer += sizeof (struct R_DATA);
+
+    if (ntohs(answers[i].resource->rtype) == T_PTR) {
+      fprintf(stderr, "\tT_PTR answer.\n");
+      answers[i].rdata = read_name_from_packet(read_pointer, consumed);
+      read_pointer = read_pointer + *consumed;
+      fprintf(stderr, "\tRdata name: %s.\n", answers[i].rdata);
+      handle_PTR_answer(&answers[i]);
+    }
+    else if (ntohs(answers[i].resource->rtype) == T_A) {
+      fprintf(stderr, "\tT_A answer.\n");
+      answers[i].rdata = malloc(ntohs(answers[i].resource->data_len));
+      for (j = 0; j < ntohs(answers[i].resource->data_len); j++) {
+        answers[i].rdata[j] = read_pointer[j];
+      }
+      answers[i].rdata[ntohs(answers[i].resource->data_len)] = '\0';
+      read_pointer = read_pointer + ntohs(answers[i].resource->data_len);
+      handle_A_answer(&answers[i]);
+    }
+    else {
+      fprintf(stderr, "Unsupported or unknown resource type\n.\
+                        Next resource records in this frame skipped\n");
+      return;
+    }
+  }
 }
 
 void read_mcast_data(evutil_socket_t sock, short events, void *arg) {
@@ -85,120 +168,11 @@ void read_mcast_data(evutil_socket_t sock, short events, void *arg) {
   fprintf(stderr, "The response contains : ");
   fprintf(stderr, "\n %d Questions.", ntohs(dns->q_count));
   fprintf(stderr, "\n %d Answers.\n", ntohs(dns->ans_count));
-  
-  char * read_pointer = &buf[sizeof(struct DNS_HEADER)];
 
-  int i, j, consumed = 0;
-  struct QUERY questions[10];
-  struct RES_RECORD answers[10];      
-  struct event_base *base = (struct event_base *)arg;
-  
-  for (i = 0; i < ntohs(dns->q_count); i++) {
-    fprintf(stderr, "Question nr %d analyzed:\n", i + 1);
-    questions[i].name = read_name_from_packet(read_pointer, buf, &consumed);
-    read_pointer += consumed;
-    fprintf(stderr, "\tName: %s\n", questions[i].name);
-    
-    questions[i].ques = (struct QUESTION *) (read_pointer);
-    if (ntohs(questions[i].ques->qtype) == T_PTR) {
-      fprintf(stderr, "\tT_PTR question.\n");
-      struct event *an_event =
-        event_new(base, write_MDNS_sock, EV_WRITE, send_PTR_answer, NULL);
-      if(!an_event) syserr("Error creating event.");
-      if(event_add(an_event, NULL) == -1) syserr("Error adding an event to a base.");
-    } else if (ntohs(questions[i].ques->qtype) == T_A) {
-      fprintf(stderr, "\tT_A question.\n");
-      
-    } else {
-      fprintf(stderr, "Unsupported or unknown resource type\n.\
-                        Next resource records in this frame skipped\n");
-      return;
-    }
-  }
+  char * read_pointer = &buf[sizeof (struct DNS_HEADER)];
+  int consumed = 0;
+  struct event_base *base = (struct event_base *) arg;
 
-  for (i = 0; i < ntohs(dns->ans_count); i++) {
-    fprintf(stderr, "Answer nr %d analyzed:\n", i + 1);
-    answers[i].name = read_name_from_packet(read_pointer, buf, &consumed);
-    read_pointer += consumed;
-
-    fprintf(stderr, "\tName: %s\n", answers[i].name);
-    
-    answers[i].resource = (struct R_DATA *) (read_pointer);
-    read_pointer += sizeof (struct R_DATA);
-    
-    if (ntohs(answers[i].resource->rtype) == T_PTR) {
-      fprintf(stderr, "\tT_PTR answer.\n");
-      
-      answers[i].rdata = read_name_from_packet(read_pointer, buf, &consumed);
-      read_pointer = read_pointer + consumed;
-      
-      fprintf(stderr, "\tRdata name: %s.\n", answers[i].rdata);
-      
-    } else if (ntohs(answers[i].resource->rtype) == T_A) {
-      fprintf(stderr, "\tT_A answer.\n");
-      answers[i].rdata = malloc(ntohs(answers[i].resource->data_len));
-
-      for (j = 0; j < ntohs(answers[i].resource->data_len); j++) {
-        answers[i].rdata[j] = read_pointer[j];
-      }
-
-      answers[i].rdata[ntohs(answers[i].resource->data_len)] = '\0';
-
-      read_pointer = read_pointer + ntohs(answers[i].resource->data_len);
-    } else  {
-      fprintf(stderr, "Unsupported or unknown resource type\n.\
-                        Next resource records in this frame skipped\n");
-      return;
-    }
-  }
-}
-
-void append_in_dns_name_format(char * dns, char * host) {
-  //dns name format: 3wwwm5imuw3edu2pl0 
-  strcat(host, ".");
-
-  int last_appended = 0, i, len = strlen(host);
-  fprintf(stderr, "append, len: %d host: %s\n", len, host);
-  for (i = 0; i < len; ++i) {
-    if (host[i] == '.') {
-      fprintf(stderr, "dot %d\n", i - last_appended);
-      *dns++ = i - last_appended;
-      for (; last_appended < i; ++last_appended) {
-        *dns++ = host[last_appended];
-        fprintf(stderr, "%c", *(--dns)); dns++;
-      }
-      ++last_appended;
-    }
-  }
-  *dns++ = '\0';
-}
-
-char * read_name_from_packet(char * read_pointer, char * buffer, int * count) {
-  char * name;
-  int p = 0;
-  *count = 1;
-  name = (char *) malloc(256);
-  name[0] = '\0';
-  
-  //read names in 3www5mimuw3edu2pl0 format
-  while (*read_pointer != 0) {
-    name[p++] = *read_pointer;
-    read_pointer = read_pointer + 1;
-    *count = *count + 1;
-  }
-
-  name[p] = '\0';
-
-  //convert 3www5mimuw3edu2pl0 to www.mimuw.edu.pl
-  int i, j;
-  for (i = 0; i < (int) strlen((const char*) name); ++i) {
-    p = name[i];
-    for (j = 0; j < (int) p; ++j) {
-      name[i] = name[i + 1];
-      i = i + 1;
-    }
-    name[i] = '.';
-  }
-  name[i - 1] = '\0'; //remove the last dot
-  return name;
+  handle_questions(dns, read_pointer, buf, &consumed, base);
+  handle_answers(dns, read_pointer, buf, &consumed, base);
 }
--- util.c
+++ util.c
@@ -10,7 +10,7 @@ struct sockaddr_in get_ip(evutil_socket_t sock) {
 }
 
 void get_hostname(char * ret_name) {
-  struct addrinfo hints, *info, *p;
+  struct addrinfo hints, *info;
   int gai_result;
 
   char hostname[1024];
@@ -32,4 +32,55 @@ void get_hostname(char * ret_name) {
   }
   ret_name[strlen(info->ai_canonname)] = '\0';
   freeaddrinfo(info);
+}
+
+void append_in_dns_name_format(char * dns, char * host) {
+  //dns name format: 3wwwm5imuw3edu2pl0 
+  strcat(host, ".");
+
+  int last_appended = 0, i, len = strlen(host);
+  fprintf(stderr, "append, len: %d host: %s\n", len, host);
+  for (i = 0; i < len; ++i) {
+    if (host[i] == '.') {
+      fprintf(stderr, "dot %d\n", i - last_appended);
+      *dns++ = i - last_appended;
+      for (; last_appended < i; ++last_appended) {
+        *dns++ = host[last_appended];
+        fprintf(stderr, "%c", *(--dns));
+        dns++;
+      }
+      ++last_appended;
+    }
+  }
+  *dns++ = '\0';
+}
+
+char * read_name_from_packet(char * read_pointer, int * count) {
+  char * name;
+  int p = 0;
+  *count = 1;
+  name = (char *) malloc(256);
+  name[0] = '\0';
+
+  //read names in 3www5mimuw3edu2pl0 format
+  while (*read_pointer != 0) {
+    name[p++] = *read_pointer;
+    read_pointer = read_pointer + 1;
+    *count = *count + 1;
+  }
+
+  name[p] = '\0';
+
+  //convert 3www5mimuw3edu2pl0 to www.mimuw.edu.pl
+  int i, j;
+  for (i = 0; i < (int) strlen((const char*) name); ++i) {
+    p = name[i];
+    for (j = 0; j < (int) p; ++j) {
+      name[i] = name[i + 1];
+      i = i + 1;
+    }
+    name[i] = '.';
+  }
+  name[i - 1] = '\0'; //remove the last dot
+  return name;
 }
